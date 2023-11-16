e5a91182832aea3b5c1707476367854fc861aaaf
adrianlopez26 <151023952+adrianlopez26@users.noreply.github.com>
2023-11-16 16:17:16 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,35 +1,60 @@
 import tienda as t
+from colorama import Fore, Style
+import pyfiglet
+
+def imprimir_titulo_bonito(titulo):
+    ascii_art = pyfiglet.figlet_format(titulo, font="big", width=150)
+    print(f"{Fore.BLUE}{ascii_art.rstrip()}{Style.RESET_ALL}", end='')
+
+titulo_bienvenida = "¡Adrian's Elegance Emporium!"
+imprimir_titulo_bonito(titulo_bienvenida)
 
 def preguntar_opcion():
     while True:
-        opcion = input("\n¿Qué deseas hacer? (Continuar/Volver/Salir): ").lower()
-        if opcion in ['continuar', 'volver', 'salir']:
+        opcion = input(f"{Fore.BLUE}\n¿Qué deseas hacer? (1.Continuar/2.Volver/3.Salir):{Style.RESET_ALL} ").lower()
+        if opcion in ['1', '2', '3']:
             return opcion
         else:
-            print("Opción no válida. Por favor, elige entre 'Continuar', 'Volver' o 'Salir'.")
+            print("Opción no válida. Por favor, elige entre '1.Continuar', '2.Volver' o '3.Salir'.")
 if __name__ == '__main__':
-    print('¡Bienvenido a la Tienda!')
     t.bienvenida()
     registro = t.RegistroCliente()
+    registro.iniciar_registro()
     registro.obtener_datos_cliente()
     registro.mostrar_datos_cliente()
     while True:
         opcion = preguntar_opcion()
-        if opcion == 'continuar':
+        if opcion == '1':
             lista_deseos_cliente = t.ListaDeDeseos(t.productos)
             lista_deseos_cliente.mostrar_productos_disponibles()
             lista_deseos_cliente.simular_agregar_a_lista()
             lista_deseos_cliente.mostrar_lista()
+
+
+            lista_deseos_cliente.mostrar_productos_disponibles()
+
             metodo_pago = t.MetodoDePago(t.paises_iva)
             pais_cliente = registro.datos_cliente.get("Pais", "España")
             metodo_pago.procesar_pago(lista_deseos_cliente.lista_deseos, pais_cliente)
             seguimiento = t.SeguimientoPaquetes()
             seguimiento.solicitar_seguimiento()
-        elif opcion == 'volver':
-            continue
-        elif opcion == 'salir':
+
+            # Restaura la lista de productos a la original después de simular agregar a la lista
+            lista_deseos_cliente.restaurar_productos_originales()
             break
 
+        elif opcion == '2':
+            continue
+        elif opcion == '3':
+            break
 
+'''
+{Fore.RED}
+{Fore.GREEN}
+{Fore.BLUE}
+{Fore.CYAN}
+{Fore.MAGENTA}
 
+{Style.RESET_ALL}
+'''
 
--- tienda.py
+++ tienda.py
@@ -1,8 +1,10 @@
 import random
+from colorama import Fore, Style
+from tabulate import tabulate
 def bienvenida():
     mensaje = """
-    ¡Bienvenid@ a "Tu Tienda Favorita"!   
-    Gracias por elegir "Tu Tienda Favorita". ¡Esperamos que disfrutes de tu visita!
+    ¡Bienvenid@ a Adrian's Elegance Emporium!   
+    ¡Esperamos que disfrutes de tu visita!
     """
     print(mensaje)
 
@@ -10,36 +12,40 @@ class RegistroCliente:
     def __init__(self):
         self.datos_cliente = {}
 
+    def iniciar_registro(self):
+        respuesta = input(
+            f"{Fore.GREEN}¿Deseas iniciar tu registro en 'Tu Tienda Favorita'? (Sí/No): {Style.RESET_ALL}").lower()
+        if respuesta != 'si':
+            print(f"{Fore.RED}¡Gracias por visitarnos! Hasta luego.{Style.RESET_ALL}")
+            exit()
+
     def obtener_datos_cliente(self):
-        print("Por favor, ingrese la siguiente información:")
+        print(f"{Fore.MAGENTA}Vamos a comenzar el registro, ingrese la siguiente información:{Style.RESET_ALL}")
         while True:
-            nombre = input("Nombre: ")
-            passwd = input("Contraseña: ")
-            pais = input("Pais: ").lower()
-            correo = input("Correo electrónico: ")
-            if not nombre or not passwd or not pais or not correo:
-                print("Por favor, completa todos los campos obligatorios.")
-                continue
-            if '@' not in correo:
-                print("El correo electrónico debe contener '@'.")
+            nombre = input(f"{Fore.GREEN}Nombre:{Style.RESET_ALL} ")
+            passwd = input(f"{Fore.GREEN}Contraseña:{Style.RESET_ALL} ")
+            print(f"{Fore.BLUE}Paises que importamos:\n 1.España, 2.Alemania, 3.Belgica, 4.Francia{Style.RESET_ALL}")
+            pais = input(f"{Fore.GREEN}Pais:{Style.RESET_ALL} ").lower()
+            if not nombre or not passwd or not pais:
+                print(f"{Fore.RED}Por favor, completa todos los campos obligatorios.{Style.RESET_ALL}")
                 continue
             self.datos_cliente = {
                 "Nombre": nombre,
                 "Contraseña": passwd,
                 "Pais": pais,
-                "Correo electrónico": correo
             }
             break
-
     def mostrar_datos_cliente(self):
-        print("\n¡Registro exitoso! Estos son los datos que proporcionaste:")
+        print(f"{Fore.GREEN}\n¡Registro exitoso! Estos son los datos que proporcionaste:{Style.RESET_ALL}")
         for clave, valor in self.datos_cliente.items():
             print(f"{clave}: {valor}")
+
 
 class ListaDeDeseos:
     def __init__(self, productos):
         self.lista_deseos = []
-        self.productos = productos
+        self.productos = productos.copy()
+        self.productos_originales = productos.copy()
 
     def agregar_a_lista(self, codigo_producto, cantidad=1):
         producto_seleccionado = self.productos.get(codigo_producto)
@@ -48,42 +54,48 @@ class ListaDeDeseos:
                 if item['producto']['codigo'] == codigo_producto:
                     item['cantidad'] += cantidad
                     print(f"{cantidad} unidades de {item['producto']['nombre']} añadidas a la lista de deseos.")
+                    self.productos[codigo_producto]['unidades'] -= cantidad
                     return
 
-            # Si el producto no está en la lista de deseos, agregarlo
             self.lista_deseos.append({'producto': producto_seleccionado, 'cantidad': cantidad})
             print(f"{cantidad} unidades de {producto_seleccionado['nombre']} añadidas a la lista de deseos.")
+            self.productos[codigo_producto]['unidades'] -= cantidad
         else:
-            print("Código de producto no válido. Por favor, selecciona un código válido.")
+            print(f"{Fore.RED}Código de producto no válido. Por favor, selecciona un código válido.{Style.RESET_ALL}")
 
     def mostrar_productos_disponibles(self):
-        print("Productos Disponibles:")
+        productos_tabla = []
         for codigo, producto_info in self.productos.items():
-            print(f"Código {codigo}: {producto_info['nombre']} - Precio: ${producto_info['precio']} - Unidades disponibles: {producto_info['unidades']}")
+            productos_tabla.append([codigo, producto_info['nombre'], f"${producto_info['precio']}", producto_info['unidades']])
+
+        headers = [f"{Fore.BLUE}Código{Style.RESET_ALL}", f"{Fore.BLUE}Nombre{Style.RESET_ALL}", f"{Fore.BLUE}Precio{Style.RESET_ALL}", f"{Fore.BLUE}Unidades{Style.RESET_ALL}"]
+        print(tabulate(productos_tabla, headers=headers, tablefmt="fancy_grid"))
 
     def simular_agregar_a_lista(self):
-        # Simular que el cliente agrega productos a la lista de deseos
         while True:
             try:
-                codigo_seleccionado = int(input("\nSelecciona un producto ingresando su código (o 0 para salir): "))
+                codigo_seleccionado = int(input(f"{Fore.MAGENTA}\nSelecciona un producto ingresando su código (o 0 para salir):{Style.RESET_ALL} "))
                 if codigo_seleccionado == 0:
                     break
 
                 if codigo_seleccionado in self.productos:
-                    cantidad_seleccionada = int(input(f"¿Cuántas unidades de {self.productos[codigo_seleccionado]['nombre']} deseas agregar? "))
+                    cantidad_seleccionada = int(input(f"{Fore.MAGENTA}¿Cuántas unidades de {self.productos[codigo_seleccionado]['nombre']} deseas agregar?{Style.RESET_ALL} "))
                     self.agregar_a_lista(codigo_seleccionado, cantidad_seleccionada)
                 else:
-                    print("Código no válido. Por favor, selecciona un código válido.")
+                    print(f"{Fore.RED}Código no válido. Por favor, selecciona un código válido.{Style.RESET_ALL}")
             except ValueError:
-                print("Ingresa un número válido.")
+                print(f"{Fore.RED}Ingresa un número válido.{Style.RESET_ALL}")
 
     def mostrar_lista(self):
         if not self.lista_deseos:
-            print("Tu lista de deseos está vacía.")
+            print(f"{Fore.RED}Tu lista de deseos está vacía.{Style.RESET_ALL}")
         else:
-            print("Tu lista de deseos:")
+            print(f"{Fore.BLUE}Tu lista de deseos:{Style.RESET_ALL}")
             for item in self.lista_deseos:
-                print(f"{item['cantidad']} unidades de {item['producto']['nombre']}")
+                print(f"{Fore.MAGENTA}{item['cantidad']} unidades de {item['producto']['nombre']}{Style.RESET_ALL}")
+
+    def restaurar_productos_originales(self):
+        self.productos = self.productos_originales.copy()
 
 # Definir productos
 productos = {
@@ -108,6 +120,14 @@ class MetodoDePago:
     def obtener_iva(self, pais):
         return self.paises_iva.get(pais, 0.21)
 
+    def obtener_correo_valido(self):
+        while True:
+            correo_cliente = input(f"{Fore.GREEN}Por favor, ingrese su correo electrónico para enviar la factura: {Style.RESET_ALL}")
+            if '@' in correo_cliente and '.' in correo_cliente:
+                return correo_cliente
+            else:
+                print(f"{Fore.RED}El correo electrónico debe contener al menos un '@' y al menos un '.'{Style.RESET_ALL}")
+
     def procesar_pago(self, lista_deseos, pais_cliente):
         iva = self.obtener_iva(pais_cliente)
 
@@ -119,16 +139,15 @@ class MetodoDePago:
             total += subtotal
 
         total_con_iva = total * (1 + iva)
-        print(f"\nTotal de la compra con IVA ({iva * 100}%): ${total_con_iva:.2f}")
-        print("¡Gracias por tu compra!")
-        print("Se le ha enviado un correo con un PDF con la factura de su pedido.")
+        correo_cliente = self.obtener_correo_valido()
+        print(f"{Fore.MAGENTA}\nTotal de la compra con IVA ({iva * 100}%): €{total_con_iva:.2f}{Style.RESET_ALL}")
+        print(f"{Fore.BLUE}¡Gracias por tu compra! Se le ha enviado un correo a {correo_cliente} con un PDF con la factura de su pedido.{Style.RESET_ALL}")
 
-# Lista de países con sus respectivos IVAs
 paises_iva = {
-    "España": 0.21,
-    "Alemania": 0.15,
-    "Belgica": 0.05,
-    "Francia": 0.18,
+    "1": 0.21,
+    "2": 0.15,
+    "3": 0.05,
+    "4": 0.18,
 }
 
 class SeguimientoPaquetes:
@@ -136,18 +155,28 @@ class SeguimientoPaquetes:
         self.codigo_seguimiento = None
 
     def generar_codigo_seguimiento(self):
-        return ''.join(random.choice('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789') for _ in range(10))
+        return ''.join(random.choice(f'{Fore.GREEN}ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789{Style.RESET_ALL}') for _ in range(10))
+
+    def obtener_telefono_valido(self):
+        while True:
+            telefono_cliente = input(f"{Fore.GREEN}Por favor, ingrese su número de teléfono para recibir el código de seguimiento: {Style.RESET_ALL}")
+            if telefono_cliente.isdigit() and len(telefono_cliente) >= 9:
+                return telefono_cliente
+            else:
+                print(f"{Fore.RED}Número de teléfono no válido. Asegúrese de ingresar un número con al menos 9 dígitos.{Style.RESET_ALL}")
 
     def solicitar_seguimiento(self):
         respuesta = input("¿Desea obtener un código de seguimiento para su paquete? (Sí/No): ").lower()
 
         if respuesta == 'si':
             self.codigo_seguimiento = self.generar_codigo_seguimiento()
-            print(f"\nGracias por su compra. Su código de seguimiento es: {self.codigo_seguimiento}")
+            telefono_cliente = self.obtener_telefono_valido()
+            print(f"{Fore.GREEN}\nGracias por su compra. Su código de seguimiento se ha enviado a su número de teléfono: {telefono_cliente}.{Style.RESET_ALL}")
         elif respuesta == 'no':
-            print("\nGracias por su compra. No se proporcionará un código de seguimiento.")
+            print(f"{Fore.GREEN}\nGracias por su compra. No se proporcionará un código de seguimiento.{Style.RESET_ALL}")
         else:
-            print("\nRespuesta no válida. Por favor, responda con 'Sí' o 'No'.")
+            print(f"{Fore.RED}\nRespuesta no válida. Por favor, responda con 'Sí' o 'No'.{Style.RESET_ALL}")
+
 
 
 
